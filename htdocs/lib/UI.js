92e6f3566267cbed95aa86116576757b5de76cc5
Christopher Andrews <godarklight@gmail.com>
2024-05-21 13:33:03 +1000

    Unbreak muting

--- htdocs/lib/UI.js
+++ htdocs/lib/UI.js
@@ -78,6 +78,9 @@ UI.setVolume = function(x) {
         //Map 0-150 to -60 to 0db gain
         xdb = (x / 2.5)  - 60;
         gain = Math.pow(10, xdb / 20);
+        if (x == 0) {
+            gain = 0;
+        }
         if (audioEngine) audioEngine.setVolume(gain);
     }
 };
