bca295d500ba9d39246cd73c0173f341024113da
Marat Fayzullin <luarvique@gmail.com>
2025-12-20 23:16:44 -0500

    Initial WiFi settings implementation.

--- owrx/controllers/settings/wifi.py
+++ owrx/controllers/settings/wifi.py
@@ -31,29 +31,34 @@ class WifiSettingsController(SettingsFormController):
     def getSections(self):
         return [
             Section(
-                "Access Point 1",
-                CheckboxInput("wifi_enable_1", "Enable this access point"),
+                "Self-Hosted Access Point",
+                TextInput("wifi_name_ap", "Name"),
+                TextInput("wifi_pass_ap", "Password"),
+            ),
+            Section(
+                "Connection 1",
+                CheckboxInput("wifi_enable_1", "Enable this connection"),
                 #WifiNameInput("wifi_name_1", "Name"),
                 TextInput("wifi_name_1", "Name"),
                 TextInput("wifi_pass_1", "Password"),
             ),
             Section(
-                "Access Point 2",
-                CheckboxInput("wifi_enable_2", "Enable this access point"),
+                "Connection 2",
+                CheckboxInput("wifi_enable_2", "Enable this connection"),
                 #WifiNameInput("wifi_name_2", "Name"),
                 TextInput("wifi_name_2", "Name"),
                 TextInput("wifi_pass_2", "Password"),
             ),
             Section(
-                "Access Point 3",
-                CheckboxInput("wifi_enable_3", "Enable this access point"),
+                "Connection 3",
+                CheckboxInput("wifi_enable_3", "Enable this connection"),
                 #WifiNameInput("wifi_name_3", "Name"),
                 TextInput("wifi_name_3", "Name"),
                 TextInput("wifi_pass_3", "Password"),
             ),
             Section(
-                "Access Point 4",
-                CheckboxInput("wifi_enable_4", "Enable this access point"),
+                "Connection 4",
+                CheckboxInput("wifi_enable_4", "Enable this connection"),
                 #WifiNameInput("wifi_name_4", "Name"),
                 TextInput("wifi_name_4", "Name"),
                 TextInput("wifi_pass_4", "Password"),
@@ -63,16 +68,5 @@ class WifiSettingsController(SettingsFormController):
     def processData(self, data):
         # Save data to config
         super().processData(data)
-        # Delete all access points added by OpenWebRX
-        wifi = WiFi()
-        wifi.clearAll()
-        # Add active WiFi access points from config
-        pm = Config()
-        if pm["wifi_enable_1"] and len(pm["wifi_name_1"]) > 0 and len(pm["wifi_pass_1"]) > 0:
-            wifi.add(pm["wifi_name_1"], pm["wifi_pass_1"])
-        if pm["wifi_enable_2"] and len(pm["wifi_name_2"]) > 0 and len(pm["wifi_pass_2"]) > 0:
-            wifi.add(pm["wifi_name_2"], pm["wifi_pass_2"])
-        if pm["wifi_enable_3"] and len(pm["wifi_name_3"]) > 0 and len(pm["wifi_pass_3"]) > 0:
-            wifi.add(pm["wifi_name_3"], pm["wifi_pass_3"])
-        if pm["wifi_enable_4"] and len(pm["wifi_name_4"]) > 0 and len(pm["wifi_pass_4"]) > 0:
-            wifi.add(pm["wifi_name_4"], pm["wifi_pass_4"])
+        # Apply new WiFi settings
+        WiFi.getSharedInstance().applyNewSettings()
---
+++ owrx/wifi.py
@@ -0,0 +1,153 @@
+from owrx.config import Config
+import subprocess
+import threading
+import time
+import re
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class WiFi(object):
+    sharedInstance = None
+    creationLock = threading.Lock()
+
+    # Return a global instance of the WiFi manager.
+    @staticmethod
+    def getSharedInstance():
+        with WiFi.creationLock:
+            if WiFi.sharedInstance is None:
+                WiFi.sharedInstance = WiFi()
+        return WiFi.sharedInstance
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.event = threading.Event()
+        self.thread = None
+        self.checkPeriod = 60
+
+    def startConnectionCheck(self):
+        # Stop existing connection check, if present
+        if self.thread is not None:
+            self.event.set()
+            while self.thread is not None:
+                time.sleep(1)
+        # Start delayed connection check
+        self.thread = threading.Thread(target=self._connectionThread, name=type(self).__name__ + ".Check")
+        self.thread.start()
+
+    def startHotspot(self, ssid: str = "openwebrx", password: str = "openwebrx", device: str = "wlan0"):
+        if len(ssid) > 0 and len(password) > 0 and len(device) > 0:
+            command = [
+                "nmcli", "device", "wifi", "hotspot",
+                "con-name", "owrx.hotspot",
+                "ifname", device,
+                "ssid", ssid,
+                "password", password
+            ]
+            try:
+                subprocess.run(command, check=True)
+                return True
+            except Exception as e:
+                logger.error("Failed to start hotspot '{0}': {1}".format(ssid, str(e)))
+        return False
+
+    def stopHotspot(self):
+        return self.delete("owrx.hotspot")
+
+    def getCurrentSSID(self, device: str = "wlan0"):
+        command = ["nmcli", "-t", "-c", "no", "device", "status"]
+        try:
+            result = subprocess.run(command, capture_output=True, text=True, check=True)
+            lines = result.stdout.splitlines()
+            for line in lines:
+                m = re.match(r"^(.*):(.*):(.*):(.*)\s*$")
+                if m is not None and m.group(1) == device and m.group(3) == "connected":
+                    return m.group(4)
+        except Exception as e:
+            logger.error("Failed to get connected SSID: " + str(e))
+        return None
+
+    def getAll(self):
+        command = ["nmcli", "-t", "-c", "no", "con", "show"]
+        out = []
+        try:
+            result = subprocess.run(command, capture_output=True, text=True, check=True)
+            lines = result.stdout.splitlines()
+            for line in lines:
+                m = re.match(r"^(.*):(.*):(.*):(.*)\s*$", line)
+                if m is not None and m.group(3) == "802-11-wireless":
+                    name = m.group(1)
+                    uuid = m.group(2)
+                    type = m.group(3)
+                    dev  = m.group(4)
+                    out.append({ "name": name, "uuid": uuid })
+        except Exception as e:
+            logger.error("Failed to get connections: " + str(e))
+        return out
+
+    def add(self, ssid: str, password: str, device: str = "wlan0"):
+        if len(ssid) > 0 and len(password) > 0 and len(device) > 0:
+            command = [
+                "nmcli", "con", "add",
+                "con-name", "owrx." + ssid,
+                "ifname", device,
+                "type", "wifi",
+                "ssid", ssid,
+                "wifi-sec.key-mgmt", "wpa-psk",
+                "wifi-sec.psk", password,
+                "connection.autoconnect", "yes"
+            ]
+            logger.info("Adding connection '{0}'...".format(ssid))
+            try:
+                subprocess.run(command, check=True)
+                return True
+            except Exception as e:
+                logger.error("Failed to add connection '{0}': {1}".format(ssid, str(e)))
+        return False
+
+    def delete(self, name: str):
+        if len(name) > 0:
+            command = [ "nmcli", "con", "delete", name ]
+            logger.info("Deleting connection '{0}'...".format(name))
+            try:
+                subprocess.run(command, check=True)
+                return True
+            except Exception as e:
+                logger.error("Failed to delete connection '{0}': {1}".format(name, str(e)))
+        return False
+
+    def applyNewSettings(self):
+        # Delete all connections added by OpenWebRX, including hotspot
+        for ap in self.getAll():
+            if ap["name"].startswith("owrx."):
+                self.delete(ap["uuid"])
+        # Add active WiFi connections from config
+        pm = Config()
+        on = 0
+        if pm["wifi_enable_1"] and self.add(pm["wifi_name_1"], pm["wifi_pass_1"]):
+            on += 1
+        if pm["wifi_enable_2"] and self.add(pm["wifi_name_2"], pm["wifi_pass_2"]):
+            on += 1
+        if pm["wifi_enable_3"] and self.add(pm["wifi_name_3"], pm["wifi_pass_3"]):
+            on += 1
+        if pm["wifi_enable_4"] and self.add(pm["wifi_name_4"], pm["wifi_pass_4"]):
+            on += 1
+        # If no WiFi connections added, start WiFi hotspot
+        if on > 0:
+            self.startConnectionCheck()
+        else:
+            self.startHotspot(pm["wifi_name_ap"], pm["wifi_pass_ap"])
+
+    # Wait for a while, then check if WiFi connection is active
+    # Start WiFi hotspot if there is no active WiFi connection
+    def _connectionThread(self):
+        self.event.wait(self.checkPeriod)
+        if not self.event.is_set():
+            logger.info("Checking for an active WiFi connection...")
+            if self.getCurrentSSID() is None:
+                pm = Config.get()
+                logger.info("Not active WiFi connection, becoming hotspot...")
+                self.startHotspot(pm["wifi_name_ap"], pm["wifi_pass_ap"])
+        # Thread completed
+        self.thread = None
