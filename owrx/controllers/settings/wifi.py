e96660d1c7eb184b765a9c886bd4cb1bc7b82341
Marat Fayzullin <luarvique@gmail.com>
2025-12-21 19:31:23 -0500

    WiFi settings should work now.

--- owrx/controllers/settings/wifi.py
+++ owrx/controllers/settings/wifi.py
@@ -1,18 +1,8 @@
 from owrx.wifi import WiFi
-from owrx.config import Config
 from owrx.controllers.settings import SettingsFormController
 from owrx.form.section import Section
-from owrx.form.input.wifi import WifiNameInput
-from owrx.form.input import (
-    CheckboxInput,
-    TextInput,
-    NumberInput,
-    FloatInput,
-    TextAreaInput,
-    DropdownInput,
-    Option,
-)
-from owrx.form.input.validator import RangeValidator
+from owrx.form.input.wifi import WifiSsidValidator, WifiPassValidator
+from owrx.form.input import CheckboxInput, TextInput
 from owrx.breadcrumb import Breadcrumb, BreadcrumbItem
 from owrx.controllers.settings import SettingsBreadcrumb
 
@@ -32,41 +22,38 @@ class WifiSettingsController(SettingsFormController):
         return [
             Section(
                 "Self-Hosted Access Point",
-                TextInput("wifi_name_ap", "Name"),
-                TextInput("wifi_pass_ap", "Password"),
+                TextInput("wifi_name_ap", "SSID", validator=WifiSsidValidator()),
+                TextInput("wifi_pass_ap", "Password", validator=WifiPassValidator()),
             ),
             Section(
                 "Connection 1",
                 CheckboxInput("wifi_enable_1", "Enable this connection"),
-                #WifiNameInput("wifi_name_1", "Name"),
-                TextInput("wifi_name_1", "Name"),
-                TextInput("wifi_pass_1", "Password"),
+                TextInput("wifi_name_1", "SSID", validator=WifiSsidValidator()),
+                TextInput("wifi_pass_1", "Password", validator=WifiPassValidator()),
             ),
             Section(
                 "Connection 2",
                 CheckboxInput("wifi_enable_2", "Enable this connection"),
-                #WifiNameInput("wifi_name_2", "Name"),
-                TextInput("wifi_name_2", "Name"),
-                TextInput("wifi_pass_2", "Password"),
+                TextInput("wifi_name_2", "SSID", validator=WifiSsidValidator()),
+                TextInput("wifi_pass_2", "Password", validator=WifiPassValidator()),
             ),
             Section(
                 "Connection 3",
                 CheckboxInput("wifi_enable_3", "Enable this connection"),
-                #WifiNameInput("wifi_name_3", "Name"),
-                TextInput("wifi_name_3", "Name"),
-                TextInput("wifi_pass_3", "Password"),
+                TextInput("wifi_name_3", "SSID", validator=WifiSsidValidator()),
+                TextInput("wifi_pass_3", "Password", validator=WifiPassValidator()),
             ),
             Section(
                 "Connection 4",
                 CheckboxInput("wifi_enable_4", "Enable this connection"),
-                #WifiNameInput("wifi_name_4", "Name"),
-                TextInput("wifi_name_4", "Name"),
-                TextInput("wifi_pass_4", "Password"),
+                TextInput("wifi_name_4", "SSID", validator=WifiSsidValidator()),
+                TextInput("wifi_pass_4", "Password", validator=WifiPassValidator()),
             ),
         ]
 
     def processData(self, data):
         # Save data to config
         super().processData(data)
+        super().store()
         # Apply new WiFi settings
         WiFi.getSharedInstance().applyNewSettings()
---
+++ owrx/form/input/wifi.py
@@ -0,0 +1,28 @@
+from owrx.form.input.validator import Validator
+from owrx.form.error import ValidationError
+import re
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class WifiSsidValidator(Validator):
+    def validate(self, key, value):
+        # Allow for empty SSIDs which disable connection entry
+        if len(value) > 0 and len(value) not in range(1, 33):
+            raise ValidationError(key, "WiFi SSID must have length of 1..32 characters")
+        m = re.search(r"[^0-9A-Za-z_\-\.]", value)
+        if m is not None:
+            raise ValidationError(key, "WiFi SSID cannot contain '{0}' character".format(m.group(0)))
+        pass
+
+class WifiPassValidator(Validator):
+    def validate(self, key, value):
+        # Allow for empty password which disable connection entry
+        if len(value) > 0 and len(value) not in range(8, 64):
+            raise ValidationError(key, "WiFi password must have length of 8..63 characters")
+        m = re.search(r"[^\040-\176]", value)
+        if m is not None:
+            raise ValidationError(key, "WiFi password is limited to printable characters")
+        pass
