cb877c98bad718305e5966cbda73eb8e9268a615
Circu1tI0N3rd <10991093+Circu1tI0N3rd@users.noreply.github.com>
2026-08-27 02:40:07 +0700

    Adding basic support for SX1255 Soapy driver (#115)
    * Adding basic support for SX1255 Soapy sources.
    
    * Updating class names for visibility (bug) and modifying descriptions.
    
    * Fixing conflicting class names during cloning (dumb.).
    
    * Dropping 36 and 36.864 MHz variants due to SoapySX implementation (for now).
    
    * Merging m17sx1255hat into sxceiver.
    Substantially replacing sample rate selection with clock select and prescaler se
    lect, then sample rate is calculated later and provided to CommandMapper.
    
    * Updating back-propagation of sample rate via referenced calculation
    
    ---------
    
    Co-authored-by: Maurice Woodruff Klivoslov <woodruff@ArielSnap.local>
    Co-authored-by: Hai Doan <xv2w@housepls.com>

--- owrx/feature.py
+++ owrx/feature.py
@@ -57,6 +57,7 @@ class FeatureDetector(object):
         "rtl_sdr_soapy": ["soapy_connector", "soapy_rtl_sdr"],
         "rtl_tcp": ["rtl_tcp_connector"],
         "sdrplay": ["soapy_connector", "soapy_sdrplay"],
+        "sxceiver": ["soapy_connector", "soapy_sx"],
         "elad": ["soapy_connector", "soapy_elad"],
         "mirics": ["soapy_connector", "soapy_mirics"],
         "malahit_rr": ["soapy_connector", "soapy_malahit_rr"],
@@ -357,6 +358,16 @@ class FeatureDetector(object):
         from the manufacturer.
         """
         return self._has_soapy_driver("sdrplay")
+
+    def has_soapy_sx(self):
+        """
+        The [SoapySX module for SXceiver](https://github.com/tejeez/sxxcvr)
+        module is required for interfacing with OH2EAT official SXceiver and
+        other Raspberry Pi HATs (possibly other boards as well) based around
+        SX1255 IQ Transceiver IC.  You can compile them from source given, along
+        with the necessary DTOverlay module for Raspberry Pi.
+        """
+        return self._has_soapy_driver("sx")
 
     def has_soapy_elad(self):
         """
